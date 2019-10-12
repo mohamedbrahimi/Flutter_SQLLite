447b1f607b2780fc8d29fa88f672fa16cc03c1a1
mohamedbrahimi <mohkriss3@gmail.com>
2019-10-12 23:22:15 +0100

    updated app title and home widget.

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'NoteKeeper',
+      home: NoteList(),
       theme: ThemeData(
         // This is the theme of your application.
         //
